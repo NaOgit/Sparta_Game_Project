f4f839ceef14bae6dbd99d98ee328b5656bd1c53
NaO <noukongka@spartaglobal.com>
2018-08-15 20:21:12 +0100

    working score storage for player1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-$(document).ready(function()
-{
+$(function() {
   //Words array
-  var gameWrds = ["Allegation", "Idiosyncritic", "Magnanimous" , "Perfunctory", "Paradox", "Tiresome", "Inadequate", "Jargon", "Appraised", "Reprehensible", "Outrageous", "Atrocious", "Scandalous", "Virtuous", "Dazzling", "Unimpeachable","Unassailed", "Insusceptible", "Vulnerable", "Unparagoned", "Conspicuous", "Perfidiousness", "Aggression", "Hypothesis", "Exasperation", "Reincarnation", "Unpredictable", "Parameter", "Medicore", "Frivolous", "Hippopotamus", "Comprehensive", "Blasphemy", "Psychological", "Sesquipedalian", "Genocide", "Harmony", "Exaggeration", "Jabot", "Circumvallate", "Effulgent", "Ineffable", "Fulguration", "Greensward", "Horripilation", "Lachrymal", "Empyrean", "Crapulent", "Coruscate", "Evanescent", "Fuliginous", "Hymeneal", "Perficious", "Plangent", "Prothalamium", "Suserration", "Temerarious", "Viridescent", "Zephyr", "Hypocrite"];
+  var gameWrds = ["Allegation", "Idiosyncritic", "Magnanimous" , "Perfunctory", "Paradox", "Tiresome", "Inadequate", "Jargon", "Appraised", "Reprehensible", "Outrageous", "Atrocious", "Scandalous", "Virtuous", "Dazzling", "Unimpeachable","Unassailed", "Insusceptible", "Vulnerable", "Unparagoned", "Conspicuous", "Perfidiousness", "Aggression", "Hypothesis", "Exasperation", "Reincarnation", "Unpredictable", "Parameter", "Mediocre", "Frivolous", "Hippopotamus", "Comprehensive", "Blasphemy", "Psychological", "Sesquipedalian", "Genocide", "Harmony", "Exaggeration", "Jabot", "Circumvallate", "Effulgent", "Ineffable", "Fulguration", "Greensward", "Horripilation", "Lachrymal", "Empyrean", "Crapulent", "Coruscate", "Evanescent", "Fuliginous", "Hymeneal", "Perficious", "Plangent", "Prothalamium", "Suserration", "Temerarious", "Viridescent", "Zephyr", "Hypocrite"];
   //declaring global variables
   var displayWord = random();
-  var points = 0;
+  var currentScore = 0;
   var seconds = 60;
   var spark = new
   //countdown function is declared
   //Game will run when the the countdown starts
   function countdown() {
-    points = 0;
+    currentScore = 0;
     //setting time interval for gameplay
     var timer = setInterval(function(){
     //counting down
@@ -18,14 +17,20 @@ $(document).ready(function()
       $(".time").html(seconds);
       if (seconds === 0) {
         //once timer reached 0, send alert
-        alert("Game over! Your score is " + points);
+        $(".announce").html("Game over! Your score is " + currentScore);
+        // leaderBoard();
+        localStorage.setItem("score", currentScore);
+        var storedScore = localStorage.getItem("score");
+        $("#result").html(storedScore);
         $(".score").html("0");
         $(".words").html("");
         clearInterval(timer);
         seconds = 60;
         $(".time").html("60");
+        playGame();
       }
     }, 1000);
+
   }
 
   //Generate random words from array
@@ -59,6 +64,7 @@ $(document).ready(function()
   $(".words").html(formatWord());
   var currentLetterId = 0;
 
+
   //Getting keyboard to give inputs
   $(document).keydown(function(e){
     //Getting letter from the appended spans
@@ -75,9 +81,9 @@ $(document).ready(function()
     }
     //If input letters are the same as letters from array
     if(String.fromCharCode(keyinput) == currentLetter.html()){
-      //Increment points
-      points++;
-      $(".score").html(points);
+      //Increment currentScore
+      currentScore++;
+      $(".score").html(currentScore);
       currentLetterId++;
     } else {
       currentLetter.removeClass("bg");
@@ -91,4 +97,17 @@ $(document).ready(function()
       currentLetterId = 0;
     }
   });
+
+  // function playGame() {
+  //   countdown();
+  //   random();
+  //   $(".words").html(displayWord);
+  //   getArray(word);
+  //   formatWord();
+  //   $(".words").html(formatWord());
+  //   var currentLetterId = 0;
+  // }
+
+
+
 });
